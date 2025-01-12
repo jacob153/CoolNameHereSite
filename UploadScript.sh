a021c2f95cf14286b86a2643ba59c4d73d579657
Jacob153 <jacobahuffine@gmail.com>
2025-01-12 12:19:52 -0500

    Sun Jan 12 12:19:52 PM EST 2025

---
+++ UploadScript.sh
@@ -0,0 +1,7 @@
+git add .
+CommitMessage=$(date)
+git commit -m "$CommitMessage"
+git push -u origin main
+git subtree split --prefix public -b hostinger-deploy
+git push origin hostinger-deploy:hostinger --force
+git branch -D hostinger-deploy
