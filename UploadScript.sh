06744979929ab7117afef4e3e5691b569633875a
Jacob153 <jacobahuffine@gmail.com>
2025-01-12 15:36:16 -0500

    Sun Jan 12 03:36:16 PM EST 2025

--- UploadScript.sh
+++ UploadScript.sh
@@ -6,5 +6,3 @@ git push -u origin main
 git subtree split --prefix public -b hostinger-deploy
 git push origin hostinger-deploy:hostinger --force
 git branch -D hostinger-deploy
-read -n 1 -s -r -p "Press any key to continue"
-exit
